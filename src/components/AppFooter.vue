7fe8f487f7f87ac2eea090af1dcf2c91a74d5755
Beniamin <beniamin.lyzniak@bowwe.com>
2025-06-09 19:25:18 +0200

    feat: New features

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,19 +1,51 @@
 <template>
-  <v-footer app height="40">
-
+  <v-footer app height="50">
+    <div class="siteFooter d-flex align-center justify-space-between mx-auto px-4">
+      <span>&#169; 2012 - 2025 Linkin Park - Poland</span>
+      <div class="siteFooter_socials d-flex align-center">
+        <v-icon :size="24" icon="mdi-youtube" @click="openYoutubeChannel"></v-icon>
+        <v-icon :size="24" icon="mdi-facebook" @click="openFacebookPage"></v-icon>
+      </div>
+    </div>
   </v-footer>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { Component, Vue, toNative } from 'vue-facing-decorator';
 
+@Component({
+    name: 'SiteFooter'
+})
+class SiteFooter extends Vue {
+  readonly FACEBOOK_URL = 'https://www.facebook.com/LPPoland';
+  readonly YOUTUBE_URL = 'https://www.youtube.com/@LinkinParkPoland';
+
+  openFacebookPage(): void {
+    window.open(this.FACEBOOK_URL, '_blank');
+  }
+
+  openYoutubeChannel(): void {
+    window.open(this.YOUTUBE_URL, '_blank');
+  }
+}
+
+export default toNative(SiteFooter)
 </script>
 
-<style scoped lang="sass">
-  .social-link :deep(.v-icon)
-    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity))
-    text-decoration: none
-    transition: .2s ease-in-out
-
-    &:hover
-      color: rgba(25, 118, 210, 1)
+<style lang="scss">
+  .siteFooter {
+    user-select: none;
+    max-width: 900px;
+    width: 100%;
+    &_socials {
+      column-gap: 12px;
+    }
+    @media (max-width: 599px) {
+      flex-direction: column;
+      span {
+        order: 2;
+        font-size: 12px;
+      }
+    }
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="fill-height" max-width="900">
     <div class="mx-auto">
-      <v-img class="mb-4" height="150" src="@/assets/logo.png"/>
+      <v-img class="mb-4" height="150" src="@/assets/logo.webp"/>
       <div class="mb-8 text-center">
         <div class="text-body-2 font-weight-regular mb-n1">POLAND</div>
         <h1 class="text-h2 font-weight-bold">LINKIN PARK</h1>
@@ -10,29 +10,29 @@
     <div class="actionsContainer pa-4">
       <h2 class="font-weight-bold text-center mb-6">Opener Festival 2025 - Akcje koncertowe</h2>
       <div class="action d-flex">
-        <img height="150" src="@/assets/welcome-back.jpg" alt="Welcome back" class="mr-4" />
+        <img height="150" src="@/assets/welcome-back.webp" alt="Welcome back" class="mr-4" />
         <div class="d-flex flex-column justify-space-around">
           <h3 class="mb-2">Kartki "Welcome back"</h3>
           <p class="mb-2">Na początku koncertu wszyscy podnosimy kartki z napisem "WELCOME BACK", tworząc niesamowity widok dla zespołu. Pokażmy im naszą jedność, wsparcie i wdzięczność za powrót! 🖤</p>
-          <v-btn append-icon="mdi-chevron-triple-right" :ripple="false" variant="outlined">Czytaj więcej</v-btn>
+          <v-btn append-icon="mdi-chevron-triple-right" :ripple="false" variant="outlined" @click="openAction1">Czytaj więcej</v-btn>
         </div>
       </div>
       <v-divider class="my-4"/>
       <div class="action d-flex">
-        <img height="150" src="@/assets/welcome-back.jpg" alt="Welcome back" class="mr-4" />
+        <img height="150" src="@/assets/welcome-back.webp" alt="Welcome back" class="mr-4" />
         <div class="d-flex flex-column justify-space-around">
           <h3 class="mb-2">Kartki "Welcome back"</h3>
           <p class="mb-2">Na początku koncertu wszyscy podnosimy kartki z napisem "WELCOME BACK", tworząc niesamowity widok dla zespołu. Pokażmy im naszą jedność, wsparcie i wdzięczność za powrót! 🖤</p>
-          <v-btn append-icon="mdi-chevron-triple-right" :ripple="false" variant="outlined">Czytaj więcej</v-btn>
+          <v-btn append-icon="mdi-chevron-triple-right" :ripple="false" variant="outlined" @click="openAction1">Czytaj więcej</v-btn>
         </div>
       </div>
       <v-divider class="my-4"/>
       <div class="action d-flex">
-        <img height="150" src="@/assets/welcome-back.jpg" alt="Welcome back" class="mr-4" />
+        <img height="150" src="@/assets/welcome-back.webp" alt="Welcome back" class="mr-4" />
         <div class="d-flex flex-column justify-space-around">
           <h3 class="mb-2">Kartki "Welcome back"</h3>
           <p class="mb-2">Na początku koncertu wszyscy podnosimy kartki z napisem "WELCOME BACK", tworząc niesamowity widok dla zespołu. Pokażmy im naszą jedność, wsparcie i wdzięczność za powrót! 🖤</p>
-          <v-btn append-icon="mdi-chevron-triple-right" :ripple="false" variant="outlined">Czytaj więcej</v-btn>
+          <v-btn append-icon="mdi-chevron-triple-right" :ripple="false" variant="outlined" @click="openAction1">Czytaj więcej</v-btn>
         </div>
       </div>
     </div>
@@ -40,9 +40,16 @@
 </template>
 
 <script setup lang="ts">
+const ACTION_1 = 'https://www.facebook.com/LPPoland/posts/pfbid0zNfQ6ZrVDFAZ8xnr2VNVtoEveCdfi2F63UK641EK7gjUo7pmhbpHbQzmFQydvGRYl';
 
+function openAction1(): void {
+  window.open(ACTION_1, '_blank');
+}
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+.v-container {
+  user-select: none;
+}
 .actionsContainer {
   user-select: none;
   background-color: #282828;
@@ -79,6 +86,11 @@
         margin: 0 auto;
       }
     }
+  }
+}
+@media (max-width: 599px) {
+  h1.text-h2 {
+    font-size: 2.5rem !important;
   }
 }
 </style>
